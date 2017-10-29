7559a11d83c471e5f47fb86ea7cb17c26948cfa1
unknown <lhuber@STIITCSC703.intranet.epfl.ch>
2017-10-29 15:47:32 +0100

    LAB3

---
+++ LAB3/functions/inertialNavigation.m
@@ -0,0 +1,15 @@
+function [x, v] = inertialNavigation(x0, v0,acc, gyr, dT])
+% Straspdown intertial navigation
+%
+% x0 - initial position
+% v0 - initial velocity
+%
+% acc - accelerometer measurments
+% gyr - gyroscope measurments
+%
+% dT  - timeste [s]
+
+
+
+
+end
