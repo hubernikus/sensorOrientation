aea4e393edfee099f630f963ede9a495243aca33
Lukas Huber <hubernikus@gmail.com>
2017-11-10 00:26:10 +0100

    lab5 finished

--- LAB1/whiteNoiseGen.m
+++ LAB1/whiteNoiseGen.m
@@ -1,15 +1,15 @@
-function [ DATA ] = whiteNoiseGen( N, M var )
+function [ DATA ] = whiteNoiseGen(N, M, var)
 % WHITENOSIEGENERATOR 
 % White noise: x_(k+1) = w_k
 
-if nargin<3
+if nargin<2
     M =1;
-    if nargin<2
+    if nargin<3
         var = 1;
     end
 end
 
-DATA = randn(N,M)*var;
+DATA = randn(M,N)*var;
 
 end
 
